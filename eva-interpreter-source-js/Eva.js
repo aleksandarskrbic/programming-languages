d16f24b7f22a380de78c6fd8d1832d480f02d91f
aleksandarskrbic <skrbic.alexa@gmail.com>
2023-09-02 13:28:12 +0200

    refactor tests

--- eva-interpreter-source-js/Eva.js
+++ eva-interpreter-source-js/Eva.js
@@ -1,7 +1,12 @@
-const assert = require('assert');
+const Environment = require('./Environment');
 
 class Eva {
-    eval(exp) {
+    constructor(global = new Environment()) {
+        this.global = global;
+    }
+
+    eval(exp, env = this.global) {
+
         if (isNumber(exp)) {
             return exp;
         }
@@ -10,16 +15,48 @@ class Eva {
             return exp.slice(1, -1);
         }
 
-        if (exp[0] == '+') {
-            return this.eval(exp[1]) + this.eval(exp[2]);
+        if (exp[0] === '+') {
+            return this.eval(exp[1], env) + this.eval(exp[2], env);
         }
 
-        if (exp[0] == '*') {
-            return this.eval(exp[1]) * this.eval(exp[2]);
+        if (exp[0] === '*') {
+            return this.eval(exp[1], env) * this.eval(exp[2], env);
         }
 
-        throw 'Unimplemented';
+        if (exp[0] === 'begin') {
+            const blockEnv = new Environment({}, env);
+            return this._evalBlock(exp, blockEnv);
+        }
+
+        if (exp[0] === 'set') {
+            const [_, name, value] = exp;
+            return env.assign(name, this.eval(value, env));
+        }
+
+        if (exp[0] === 'var') {
+            const [_, name, value] = exp;
+            return env.define(name, this.eval(value, env));
+        }
+
+        if (isVariableName(exp)) {
+            return env.lookup(exp);
+        }
+
+        throw `Unimplemented ${JSON.stringify(exp)}`;
     }
+
+    _evalBlock(block, env) {
+        let result;
+
+        const [_tag, ...expressions] = block;
+
+        expressions.forEach(exp => {
+            result = this.eval(exp, env);
+        });
+
+        return result;
+    }
+
 }
 
 function isNumber(exp) {
@@ -30,14 +67,8 @@ function isString(exp) {
     return typeof exp === 'string' && exp[0] === '"' && exp.slice(-1) === '"';
 }
 
-// Tests
-const eva = new Eva();
+function isVariableName(exp) {
+    return typeof exp === 'string' && /^[+\-*/<>=a-zA-Z0-9_]+$/.test(exp);
+}
 
-assert.strictEqual(eva.eval(1), 1);
-assert.strictEqual(eva.eval('"string"'), 'string');
-
-assert.strictEqual(eva.eval(['+', 1, 5]), 6);
-assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10);
-assert.strictEqual(eva.eval(['+', ['*', 3, 2], 5]), 11);
-
-console.log("All tests passed!");
+module.exports = Eva;
